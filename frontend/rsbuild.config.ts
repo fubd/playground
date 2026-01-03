8523ffadf50cdc3e43965ca3bb5a5d3d38fa5900
fubd <312bdf@gmail.com>
2026-01-03 15:21:32 +0800

    feat: init

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -4,11 +4,7 @@ import { pluginReact } from '@rsbuild/plugin-react';
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginReact()],
-  source: {
-    define: {
-      'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
-    },
-  },
+
   server: {
     port: 3000,
     host: '0.0.0.0',
