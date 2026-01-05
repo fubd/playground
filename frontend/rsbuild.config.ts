e3666d36972447bbf815537d241d0a306a7821f4
fubd <312bdf@gmail.com>
2026-01-06 01:01:05 +0800

    feat: init

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -3,7 +3,11 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [
+    pluginReact({
+      fastRefresh: false,
+    }),
+  ],
 
   server: {
     port: 3000,
